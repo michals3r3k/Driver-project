2d41c226cf6c5d9a15ff87d89b76b5bc87b7c79b
michals3r3k <serek.michal0827@gmail.com>
2021-03-22 10:32:38 +0100

    Entity TrainingQuestion

--- src/main/java/pl/michals3r3k/driverproject/entity/TrainingQuestion.java
+++ src/main/java/pl/michals3r3k/driverproject/entity/TrainingQuestion.java
@@ -4,24 +4,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
 @NoArgsConstructor
 @Data
 @Entity
-public class Advice {
+public class TrainingQuestion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String description;
-    @Nullable
-    private String attachedFile;
+    private String question;
+    private String correctAnswer;
     private LocalDate dateOfPublic;
     private LocalTime timeOfPublic;
-    @OneToMany
-    private List<TrainingQuestion> questions;
 }
