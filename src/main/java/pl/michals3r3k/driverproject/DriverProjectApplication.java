4110a7a00dd870d735758c8b03f8b4657809a33e
michals3r3k <serek.michal0827@gmail.com>
2021-03-22 09:58:16 +0100

    Initial commit

---
+++ src/main/java/pl/michals3r3k/driverproject/DriverProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.michals3r3k.driverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/michals3r3k/driverproject/DriverProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michals3r3k.driverproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
