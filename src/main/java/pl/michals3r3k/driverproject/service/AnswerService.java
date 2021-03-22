e9fabbfa27062bb49aef477ae411dc1825016925
michals3r3k <serek.michal0827@gmail.com>
2021-03-22 10:55:58 +0100

    AnswerService

---
+++ src/main/java/pl/michals3r3k/driverproject/service/AnswerService.java
@@ -0,0 +1,9 @@
+package pl.michals3r3k.driverproject.service;
+
+import pl.michals3r3k.driverproject.entity.Answer;
+
+import java.util.List;
+
+public interface AnswerService {
+    List<Answer> findAll();
+}
